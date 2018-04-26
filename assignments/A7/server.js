c01c794b965373698166fbf9ee76265de147d0df
Maureen van Dobben <maureenvandobben@gmail.com>
2018-04-26 11:41:32 -0700

    Update server.js

--- assignments/A7/server.js
+++ assignments/A7/server.js
@@ -1,7 +1,12 @@
 var fs = require('fs');
 var AWS = require('aws-sdk');
-AWS.config.loadFromPath('credentialsEg.json');
+AWS.config.loadFromPath('credentials.json');
 var s3 = new AWS.S3();
+
+var Client = require('node-rest-client').Client;
+
+var MS = require("mongoskin");
+var db = MS.db("mongodb://localhost:27017/ame470");
 
 var express = require("express");
 var app = express();
@@ -55,10 +60,10 @@ app.post('/uploadFile', function(req, res){
 
     fs.readFile(tmpPath, function (err, data) {
         var params = {
-          Bucket:'ame4702018mvd',
+          Bucket:'ame470mvd',
           ACL:'public-read',
           Key: intname,
-          Body: buf,
+          Body: data,
           ServerSideEncryption : 'AES256'
         };
         s3.putObject(params, function(err, data) {
@@ -67,6 +72,77 @@ app.post('/uploadFile', function(req, res){
         });
     });
   });
+
+
+
+
+
+  app.get("/getImgData", function (req, res) {
+    var data = req.query;
+    var id = data.id;
+    db.collection("img").findOne({id:id}, function(err, result){
+          res.end(JSON.stringify(result));
+    });
+  });
+
+
+  app.get("/getAllImgs", function (req, res) {
+    db.collection('img').find().sort({id:-1}).toArray(function(err, items) {
+      console.log(err, items);
+      if(!items) items = [];
+      res.send(items);
+    });
+  });
+
+  app.get("/addImg", function (req, res) {
+    var data = req.query;
+    db.collection("img").insert(data, function(err, result){
+      res.send("1");
+    });
+  });
+
+
+  app.get("/renameImg", function (req, res) {
+    var data = req.query;
+    var newName = data.name;
+    var id = data.id;
+    console.log(newName, id);
+    db.collection("img").findOne({id:id}, function(err, result){
+        result.name = newName;
+        db.collection("img").save(result, function(err){
+          res.end("1");
+        });
+    });
+  });
+
+
+  app.get("/updateImg", function (req, res) {
+    var data = req.query;
+    var keys = Object.keys(data);
+    var id = data.id;
+
+    db.collection("img").findOne({id:id}, function(err, result){
+        console.log(id, err, result);
+        for(var i = 0; i < keys.length; i++){
+          var key = keys[i];
+          result[key] = data[key];
+        }
+
+        db.collection("img").save(result, function(err){
+          res.end("1");
+        });
+    });
+  });
+
+
+  app.get("/deleteImg", function (req, res) {
+    var data = req.query;
+    var id = data.id;
+    db.collection("img").remove({id:id}, function(err, result){
+       res.end("1");
+    });
+  });
+
 
 
 console.log("Simple static server listening at http://" + hostname + ":" + port);
