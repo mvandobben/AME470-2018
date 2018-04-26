9b3291dd558a0b126a4d0ec05f73a1ad7e60dfbe
Maureen van Dobben <maureenvandobben@gmail.com>
2018-04-25 17:13:13 -0700

    Add files via upload

--- assignments/A7/server.js
+++ assignments/A7/server.js
@@ -1,8 +1,6 @@
-var MS = require("mongoskin");
-var db = MS.db("mongodb://localhost:27017/ame470");
 var fs = require('fs');
 var AWS = require('aws-sdk');
-AWS.config.loadFromPath('./credentials.json');
+AWS.config.loadFromPath('credentialsEg.json');
 var s3 = new AWS.S3();
 
 var express = require("express");
@@ -28,7 +26,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(errorHandler());
 
 app.get("/", function (req, res) {
-    res.redirect("index.html");
+      res.redirect("/index.html");
 });
 
 app.post('/uploadImage', function(req, res){
@@ -36,11 +34,11 @@ app.post('/uploadImage', function(req, res){
     var s3Path = '/' + intname;
     var buf = new Buffer(req.body.data.replace(/^data:image\/\w+;base64,/, ""),'base64');
     var params = {
-        Bucket:'ame4702018erickfowler',
-        ACL:'public-read',
-        Key:intname,
-        Body: buf,
-        ServerSideEncryption : 'AES256'
+      Bucket:'ame4702018mvd',
+      ACL:'public-read',
+      Key: intname,
+      Body: buf,
+      ServerSideEncryption : 'AES256'
     };
     s3.putObject(params, function(err, data) {
         console.log(err);
@@ -54,14 +52,14 @@ app.post('/uploadFile', function(req, res){
     var fileType =  req.files.input.type;
     var tmpPath = req.files.input.path;
     var s3Path = '/' + intname;
-    
+
     fs.readFile(tmpPath, function (err, data) {
         var params = {
-            Bucket:'ame4702018erickfowler',
-            ACL:'public-read',
-            Key:intname,
-            Body: data,
-            ServerSideEncryption : 'AES256'
+          Bucket:'ame4702018mvd',
+          ACL:'public-read',
+          Key: intname,
+          Body: buf,
+          ServerSideEncryption : 'AES256'
         };
         s3.putObject(params, function(err, data) {
             res.end("success");
@@ -69,77 +67,7 @@ app.post('/uploadFile', function(req, res){
         });
     });
   });
-  
-app.get("/getImage", function (req, res) {
-	var data = req.query;
-	  var id = data.id;
-	  db.collection("img").findOne({id:id}, function(err, result){
-	        res.end(JSON.stringify(result));
-	  });
-});
 
-app.get("/getAllImgs", function (req, res) {
-  db.collection('img').find().toArray(function(err, items) {
-    console.log(err, items);
-    if(!items) items = [];
-    res.send(items);
-  });
-});
-
-app.get("/addImg", function (req, res) {
-  var data = req.query;
-  db.collection("img").insert(data, function(err, result){
-    res.send("1");
-  });
-});
-
-
-app.get("/renameImg", function (req, res) {
-  var data = req.query;
-  var newName = data.name;
-  var id = data.id;
-  console.log(newName, id);
-  db.collection("img").findOne({id:id}, function(err, result){
-      result.name = newName;
-      db.collection("img").save(result, function(err){
-        res.end("1");
-      });
-  });
-});
-
-app.get("/imgFilter", function (req, res) {
-	var data = req.query;
-	var keys = Object.keys(data);
-	var id = data.id;
-	
-    db.collection("img").findOne({id:id}, function(err, result){
-		
-		for(var i = 0; i < keys.length; i++){
-			var key = keys[i];
-			result[key] = data[key];
-		}
-		
-        db.collection("img").save(result, function(err){
-        res.end("1");
-        });
-});
- 
-});
-
-
-app.get("/deleteImg", function (req, res) {
-  var data = req.query;
-  var id = data.id;
-  db.collection("img").remove({id:id}, function(err, result){
-     res.end("1");
-  });
-});
-
-
-app.use(methodOverride());
-app.use(bodyParser());
-app.use(express.static(__dirname + '/public'));
-app.use(errorHandler());
 
 console.log("Simple static server listening at http://" + hostname + ":" + port);
 app.listen(port);
